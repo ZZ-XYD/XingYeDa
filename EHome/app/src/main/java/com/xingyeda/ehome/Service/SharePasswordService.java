f9a441622ae7de180ff469dfc9c3adcc1bdb04f6
xingyeda <zz_xingyeda@163.com>
2017-10-10 15:07:01 +0800

    修复参数为NULL的BUG

--- EHome/app/src/main/java/com/xingyeda/ehome/Service/SharePasswordService.java
+++ EHome/app/src/main/java/com/xingyeda/ehome/Service/SharePasswordService.java
@@ -16,11 +16,13 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.support.annotation.Nullable;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.widget.Toast;
 
 import com.xingyeda.ehome.AcivityRegister;
 import com.xingyeda.ehome.util.AESUtils;
+import com.xingyeda.ehome.util.SharedPreUtil;
 import com.xingyeda.ehome.zhibo.ActivitySharePlay;
 
 import java.util.List;
@@ -30,6 +32,7 @@ import java.util.TimerTask;
 public class SharePasswordService extends Service {
 
     String id;
+    Context mContext = this;
 
     public SharePasswordService() {
     }
@@ -42,10 +45,8 @@ public class SharePasswordService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Bundle bundle = intent.getExtras();
-        id = bundle.getString("id");
+        id = SharedPreUtil.getString(mContext, "userId", "");
         return super.onStartCommand(intent, flags, startId);
-
     }
 
     @Override
@@ -69,7 +70,6 @@ public class SharePasswordService extends Service {
                 List<ActivityManager.RunningTaskInfo> tasks = am.getRunningTasks(1);
                 if (!tasks.isEmpty()) {
                     ComponentName cn = tasks.get(0).topActivity;
-                    Log.v("SharePasswordService", cn.getPackageName());
                     if (cn.getPackageName().equals("com.xingyeda.ehome")) {
                         String sharePassword = "";
                         String deSharePassword = "";//解密后的设备号,用户ID,房间号字符串
@@ -79,41 +79,35 @@ public class SharePasswordService extends Service {
                                 ClipData.Item item = null;
                                 item = cm.getPrimaryClip().getItemAt(0);
                                 sharePassword = item.getText().toString();
-                                Log.v("SharePassword", sharePassword);
+                                if (sharePassword.contains("创享E家")) {
+                                    String password = sharePassword.substring(sharePassword.indexOf("￥"), sharePassword.lastIndexOf("￥")).toString();
+                                    if (password != null) {
+                                        String password_result = password.substring(1).trim().toString();
+                                        try {
+                                            deSharePassword = AESUtils.Decrypt(password_result, "1234567890123456");
+                                        } catch (Exception e) {
+                                            e.printStackTrace();
+                                        }
+                                        String[] info = deSharePassword.split("\\|");
 
-                                String result = sharePassword.substring(sharePassword.indexOf("￥"), sharePassword.lastIndexOf("￥")).toString();
-                                Log.v("Result", result);
-                                String result1 = result.substring(1).trim().toString();
-                                Log.v("Result1", result1);
-                                try {
-                                    deSharePassword = AESUtils.Decrypt(result1, "1234567890123456");
-                                    Log.v("DeSharePassword", deSharePassword);
-                                } catch (Exception e) {
-                                    e.printStackTrace();
+                                        String mEquipmentId = info[0];
+                                        String mId = info[1];
+                                        String mHouseNumberId = info[2];
+
+                                        if (!mId.equals(id)) {
+                                            Intent intent = new Intent();
+                                            intent.putExtra("equipmentId", mEquipmentId);
+                                            intent.putExtra("uid", id);
+                                            intent.putExtra("roomId", mHouseNumberId);
+                                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                            intent.setClass(SharePasswordService.this, ActivitySharePlay.class);
+                                            startActivity(intent);
+                                        }
+                                    }
+                                    Uri uri = Uri.parse("http://www.xyd999.com/");
+                                    ClipData cd = ClipData.newUri(getContentResolver(), "株洲市兴业达科技有限公司", uri);
+                                    cm.setPrimaryClip(cd);
                                 }
-                                String[] info = deSharePassword.split("\\|");
-
-                                String mEquipmentId = info[0];
-                                String mId = info[1];
-                                String mHouseNumberId = info[2];
-
-                                Log.v("Info", mEquipmentId + " " + mId + " " + mHouseNumberId);
-
-                                if (!mId.equals(id)) {
-                                    Intent intent = new Intent();
-                                    intent.putExtra("equipmentId", mEquipmentId);
-                                    intent.putExtra("uid", mId);
-                                    intent.putExtra("roomId", mHouseNumberId);
-                                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                                    intent.setClass(SharePasswordService.this, ActivitySharePlay.class);
-                                    startActivity(intent);
-                                }
-
-
-                                Uri uri = Uri.parse("http://www.xyd999.com/");
-                                ClipData cd = ClipData.newUri(getContentResolver(), "株洲市兴业达科技有限公司", uri);
-                                cm.setPrimaryClip(cd);
-
                             }
                         }
                     }
