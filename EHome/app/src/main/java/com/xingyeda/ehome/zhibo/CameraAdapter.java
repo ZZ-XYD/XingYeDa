c9288b87c89869d06a203b43418385692dc4868c
xingyeda <zz_xingyeda@163.com>
2017-10-30 15:53:05 +0800

    添加分享功能

--- EHome/app/src/main/java/com/xingyeda/ehome/zhibo/CameraAdapter.java
+++ EHome/app/src/main/java/com/xingyeda/ehome/zhibo/CameraAdapter.java
@@ -8,16 +8,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.ldl.imageloader.core.ImageLoader;
 import com.xingyeda.ehome.R;
 import com.xingyeda.ehome.base.ConnectPath;
+import com.xingyeda.ehome.base.LitePalUtil;
+import com.xingyeda.ehome.util.AESUtils;
+import com.xingyeda.ehome.util.SharedPreUtil;
 
 import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 
 /**
  * Created by LDL on 2017/9/18.
@@ -27,6 +32,8 @@ public class CameraAdapter extends RecyclerView.Adapter<CameraAdapter.ViewHolder
     private Context mContext;
     private List<Camera> mList;
     private ClickItem mViewClick;
+    private String mUserName;
+    private String enSharePassword;
 
     class ViewHolder extends RecyclerView.ViewHolder {
         CardView cardView;
@@ -34,6 +41,8 @@ public class CameraAdapter extends RecyclerView.Adapter<CameraAdapter.ViewHolder
         ImageView cameraImage;
         @Bind(R.id.camera_name)
         TextView cameraName;
+        @Bind(R.id.camera_share)
+        TextView cameraShare;
 
         public ViewHolder(View view) {
             super(view);
@@ -72,15 +81,55 @@ public class CameraAdapter extends RecyclerView.Adapter<CameraAdapter.ViewHolder
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        Camera camera = mList.get(position);
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+        final Camera camera = mList.get(position);
         holder.cameraName.setText(camera.getmName());
+        holder.cameraShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mUserName = LitePalUtil.getUserInfo().getmUsername();
+                String mUID = SharedPreUtil.getString(mContext, "userId", "");
+                String mEquipmentId = camera.getmEquipmentId();
+                String mRoomId = camera.getmRoomId();
+                if (mUserName != null && mUID != null && mEquipmentId != null && mRoomId != null) {
+                    String sharePassword = mEquipmentId + "|" + mUID + "|" + mRoomId;
+                    try {
+                        enSharePassword = AESUtils.Encrypt(sharePassword, "1234567890123456");
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    showShare();
+                }
+            }
+        });
         ImageLoader.getInstance().displayImage(camera.getmImagePath(), holder.cameraImage);
     }
 
     @Override
     public int getItemCount() {
         return mList.size();
+    }
+
+    private void showShare() {
+        OnekeyShare oks = new OnekeyShare();
+        oks.disableSSOWhenAuthorize();//关闭sso授权
+//        oks.setTitle(getString(R.string.share));
+        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+//        oks.setTitleUrl("http://sharesdk.cn");
+        // text是分享文本，所有平台都需要这个字段0
+        oks.setText("content", "【来自" + mUserName + "的直播观看邀请】,复制这条信息￥" + enSharePassword + "￥后打开创享E家!");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+//        oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+//        oks.setUrl("http://sharesdk.cn");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+//        oks.setComment("我是测试评论文本");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+//        oks.setSite(getString(R.string.app_name));
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+//        oks.setSiteUrl("http://sharesdk.cn");
+        // 启动分享GUI
+        oks.show(mContext);
     }
 }
 
