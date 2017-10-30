352c38e0a5347b1bfd885fa9570d7e551070931c
xingyeda <zz_xingyeda@163.com>
2017-10-30 10:57:23 +0800

    添加默认广告图

--- EHome/app/src/main/java/com/xingyeda/ehome/adapter/AdvertisingAdapter.java
+++ EHome/app/src/main/java/com/xingyeda/ehome/adapter/AdvertisingAdapter.java
@@ -6,6 +6,7 @@ import android.widget.ImageView;
 
 import com.jude.rollviewpager.RollPagerView;
 import com.jude.rollviewpager.adapter.LoopPagerAdapter;
+import com.xingyeda.ehome.R;
 import com.xingyeda.ehome.bean.AdvertisementBean;
 
 import java.util.List;
@@ -14,6 +15,7 @@ import java.util.List;
 public class AdvertisingAdapter extends LoopPagerAdapter {
 
     private List<AdvertisementBean> mList;
+    private int imgs[] = {R.mipmap.default_advertising_1, R.mipmap.default_advertising_2, R.mipmap.default_advertising_3};
 
     public AdvertisingAdapter(RollPagerView viewPager, List<AdvertisementBean> mList) {
         super(viewPager);
@@ -23,7 +25,11 @@ public class AdvertisingAdapter extends LoopPagerAdapter {
     @Override
     public View getView(ViewGroup container, int position) {
         ImageView view = new ImageView(container.getContext());
-        view.setImageBitmap(mList.get(position).getmBitmap());
+        if (mList != null) {
+            view.setImageBitmap(mList.get(position).getmBitmap());
+        } else {
+            view.setImageResource(imgs[position]);
+        }
         view.setScaleType(ImageView.ScaleType.FIT_XY);
         view.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
         return view;
@@ -31,6 +37,10 @@ public class AdvertisingAdapter extends LoopPagerAdapter {
 
     @Override
     public int getRealCount() {
-        return mList.size();
+        if (mList != null) {
+            return mList.size();
+        } else {
+            return imgs.length;
+        }
     }
 }
