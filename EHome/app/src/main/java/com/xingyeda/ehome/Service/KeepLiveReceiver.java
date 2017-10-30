60c40113d3864ece8ceafdba5466a726fd92507d
xingyeda <zz_xingyeda@163.com>
2017-10-30 10:49:03 +0800

    修改拦截动作

--- EHome/app/src/main/java/com/xingyeda/ehome/Service/KeepLiveReceiver.java
+++ EHome/app/src/main/java/com/xingyeda/ehome/Service/KeepLiveReceiver.java
@@ -18,12 +18,12 @@ public class KeepLiveReceiver extends BroadcastReceiver {
             Intent it = new Intent(context, ActivityKeepLive.class);
             it.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(it);
-        }else if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)){
+        }else if (intent.getAction().equals(Intent.ACTION_USER_PRESENT)){
             context.sendBroadcast(new Intent("finish"));
-            Intent main = new Intent(Intent.ACTION_MAIN);
-            main.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            main.addCategory(Intent.CATEGORY_HOME);
-            context.startActivity(main);
+//            Intent main = new Intent(Intent.ACTION_MAIN);
+//            main.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//            main.addCategory(Intent.CATEGORY_HOME);
+//            context.startActivity(main);
         }
     }
 }
